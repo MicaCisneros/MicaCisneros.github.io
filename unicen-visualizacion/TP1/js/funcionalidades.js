6c00f5beb9654aed2970673148217d76412dc773
MicaCisneros <micaela_cisneros@outlook.es>
2022-05-29 19:36:19 -0300

    Terminamos login/agregamos funcionalidad like/dislike

---
+++ unicen-visualizacion/TP1/js/funcionalidades.js
@@ -0,0 +1,24 @@
+document.querySelector('#like').addEventListener('click', e => {
+    e.preventDefault();
+
+    let boton = document.querySelector('#like');
+    let botonDislike = document.querySelector('#dislike');
+    botonDislike.classList.remove("iconoSeleccionDislike");
+    botonDislike.classList.add("color-blanco");
+
+    boton.classList.add("iconoSeleccion");
+    boton.classList.remove("color-blanco");
+});
+
+document.querySelector('#dislike').addEventListener('click', e => {
+    e.preventDefault();
+
+    let botonDislike = document.querySelector('#dislike');
+    let boton = document.querySelector('#like');
+    console.log(boton);
+    botonDislike.classList.add("iconoSeleccionDislike");
+    botonDislike.classList.remove("color-blanco");
+
+    boton.classList.remove("iconoSeleccion");
+    boton.classList.add("color-blanco");
+});
