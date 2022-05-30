bf3ad418ff6ab0af5eae365f959a86ef5d030228
carolina hansen <laaflaka.8@gmail.com>
2022-05-30 18:08:35 -0300

    agregue funcionalidad de ordenamiento por calificacion

--- unicen-visualizacion/TP1/js/buscador.js
+++ unicen-visualizacion/TP1/js/buscador.js
@@ -1,46 +1,46 @@
 let juegos = [{
         'nombre': 'Among Us',
-        'calificacion': 5,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego18.jfif" alt="">'
     },
     {
         'nombre': 'Trophy Challenge',
-        'calificacion': 3,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego1.jfif" alt="">'
     },
     {
         'nombre': 'Minecraft',
-        'calificacion': 4,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego10.jfif" alt="">'
     },
     {
         'nombre': 'Fortnite',
-        'calificacion': 1,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego10.jfif" alt="">'
     },
     {
         'nombre': 'Gta',
-        'calificacion': 5,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego19.jfif" alt="">'
     },
     {
         'nombre': 'Rogue',
-        'calificacion': 2,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego13.jfif" alt="">'
     },
     {
         'nombre': 'Moving Out',
-        'calificacion': 3,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego4.jfif" alt="">'
     },
     {
         'nombre': 'Gta 2',
-        'calificacion': 4,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego14.jfif" alt="">'
     },
     {
         'nombre': 'Mario Bros',
-        'calificacion': 5,
+        'calificacion': '<i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i><i class="estrella-juego fa-solid fa-star"></i>',
         'img': '<img class="imgJuegoCategoria" src="img/juego22.jfif" alt="">'
     },
 ];
@@ -60,38 +60,45 @@ document.querySelector('#select-ordenar').addEventListener('click', () => {
 
     let j = [{}];
     select = document.querySelector('#select-ordenar').value;
+    console.log(select);
     if (select == "nombre") {
-        j = juegos.sort(SortArray);
+        j = juegos.sort(function(x, y) {
+            return x.nombre.localeCompare(y.nombre);
+        });
         console.log(j);
         mostrar(j);
 
     } else
     if (select == "mejor-calificado") {
-        j = juegos.sort(SortCalificacion);
+        j = juegos.sort(function(x, y) {
+            return x.calificacion.localeCompare(y.calificacion);
+        });
         console.log(j);
 
+        j.reverse();
         mostrar(j);
     }
 
 
 })
 
-function SortArray(x, y) {
-    return x.nombre.localeCompare(y.nombre);
-}
+// function SortArray(x, y) {
+//     return x.nombre.localeCompare(y.nombre);
+// }
 
 function SortCalificacion(x, y) {
     return x.calificacion.localeCompare(y.calificacion);
 }
 
 function mostrar(games) {
+    let contenedorPrevio = document.getElementById('cont-previo');
     let contenedor = document.getElementById('cont-secc-busqueda');
     let contenido = '';
 
     games.forEach(element => {
         contenido += '  <div class="bg-gris-oscuro juegoBusqueda"> <div class="divImgJuegoCategoria">' + element.img +
-            '</div> <div class="tituloJuegoCategoria">' +
-            element.nombre + ' </div> </div>';
+            '</div> <div class="tituloJuegoCategoria"> <p>' +
+            element.nombre + '</p>' + element.calificacion + ' </div> </div>';
 
     });
 
@@ -99,6 +106,8 @@ function mostrar(games) {
 
     contenido += '</div>';
 
-    contenedor.innerHTML = contenido;
+    contenedor.style.visibility = 'hidden';
+    contenedorPrevio.classList.add("juegosSeccionBusqueda");
+    contenedorPrevio.innerHTML = contenido;
 
 }
