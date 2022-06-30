7eac91750d32005df3e4fc03af59ed4016375e09
MicaCisneros <micaela_cisneros@outlook.es>
2022-06-30 17:12:55 -0300

    Creamos funciones para generar obstaculo

--- unicen-visualizacion/TP4/EntregaFinal/js/juegoloop/main.js
+++ unicen-visualizacion/TP4/EntregaFinal/js/juegoloop/main.js
@@ -1,6 +1,4 @@
 
-
-document.addEventListener("DOMContentLoaded", function(e) {
     let personaje = document.querySelector("#personaje");
     let muerte = document.querySelector("#muerte");
     muerte.setAttribute("hidden", "");
@@ -10,16 +8,36 @@ document.addEventListener("DOMContentLoaded", function(e) {
     let topPersona = posicionPersona['top'];
     console.log(topPersona);
     let divJuego = document.querySelector("#game-loop");
-    divJuego.addEventListener('mousedown', evento => {
-        topPersona= topPersona -10;
-        personaje.style.top = topPersona+'px';
-        muerte.style.top = topPersona+'px';
-    });
+    divJuego.addEventListener('mousedown', moverPersonaje);
+
+    let bomba = document.querySelector(".divBomba");
+    let obstaculo = new Obstaculo(bomba);
+    obstaculo.generarObstaculo();
+    let timerVariable = setInterval(caerPersonaje, 500);
 
 
+    function caerPersonaje(){
+        if(verificarPerdedor()){
+            topPersona= topPersona +10;
+            personaje.style.top = topPersona+'px';
+            muerte.style.top = topPersona+'px';
+        }else{
+            juego.accionMuerte();
+        }
+    }
+    function moverPersonaje(){
+        if(verificarPerdedor()){
+            topPersona= topPersona -10;
+            personaje.style.top = topPersona+'px';
+            muerte.style.top = topPersona+'px';
+        }else{
+            juego.accionMuerte();
+        }
+    }
 
-    let timerVariable = setInterval(juego.accionMuerte, 10000);
-
-})
-
+    function verificarPerdedor(){
+        if(topPersona <= 0 || topPersona>=400 ){
+            return false
+        }else return true;
+    }
 
---
+++ unicen-visualizacion/TP4/EntregaFinal/js/juegoloop/obstaculo.js
@@ -0,0 +1,20 @@
+class Obstaculo{
+
+    constructor(bomba){
+        this.bomba=bomba;
+        this.posicion= bomba.getBoundingClientRect();
+    }
+
+    generarObstaculo(){
+        this.bomba.setAttribute("id","bomba");
+        this.bomba.style.top = Math.floor(Math.random()*(10-300+1)+300) + "px";
+        this.ocultarBomba()
+    }
+
+    ocultarBomba(){
+        if(this.posicion['y']<0){
+            this.bomba.removeAttribute("id","bomba");
+        }
+    }
+
+}
