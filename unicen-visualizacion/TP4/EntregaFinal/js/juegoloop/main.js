89c75c44ac3f1a71a31c7b5b93dc0aef36c3ffd9
MicaCisneros <micaela_cisneros@outlook.es>
2022-06-30 16:09:43 -0300

    Implementamos movimiento al personaje

--- unicen-visualizacion/TP4/EntregaFinal/js/juegoloop/main.js
+++ unicen-visualizacion/TP4/EntregaFinal/js/juegoloop/main.js
@@ -7,12 +7,13 @@ document.addEventListener("DOMContentLoaded", function(e) {
     let posicionPersona = personaje.getBoundingClientRect();
     console.log(posicionPersona);
     let juego = new Loop(personaje,muerte);
-    let topPersona = personaje.style.top;
-
+    let topPersona = posicionPersona['top'];
+    console.log(topPersona);
     let divJuego = document.querySelector("#game-loop");
     divJuego.addEventListener('mousedown', evento => {
-        topPersona -= 10;
-        personaje.style.top = topPersona;
+        topPersona= topPersona -10;
+        personaje.style.top = topPersona+'px';
+        muerte.style.top = topPersona+'px';
     });
 
 
